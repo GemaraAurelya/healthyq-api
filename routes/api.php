c7af68cdb0b4eac9da6f63583371998babb59a9a
Toni <tonnyhdyt17@gmail.com>
2024-07-09 12:34:26 +0700

    login logout route

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\DokterController;
+use App\Http\Controllers\HariController;
+use App\Http\Controllers\JamController;
+use App\Http\Controllers\JanjiTemuController;
+use App\Http\Controllers\SpesialisController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\JadwalController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::get('/hi', function () {
+    return response()->json(['message' => 'Hello, World!']);
+});
