7e4982869945d8a0b77365746d7c720501d34871
Toni <tonnyhdyt17@gmail.com>
2024-07-09 12:35:55 +0700

    tambah hari resource

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::resource('dokter', DokterController::class);
 Route::get('dokter/{dokterId}/jadwal', [JadwalController::class, 'getByDokterId']);
 Route::resource('spesialis', SpesialisController::class);
+Route::resource('hari', HariController::class);
 
 Route::get('/hi', function () {
     return response()->json(['message' => 'Hello, World!']);
